5249145848c80dea3e1ede1361b82f7349f61cff
tulsi <kumari20tulsi@gmail.com>
2023-11-24 19:59:19 +0530

    changed

--- src/components/FormGenerator.js
+++ src/components/FormGenerator.js
@@ -1,117 +1,52 @@
-// // import React, { useState, useEffect } from 'react';
-// import Form from 'react-jsonschema-form';
-
-// const FormGenerator = ({ uiSchema }) => {
-//   const [formData, setFormData] = useState({});
-//   const [schema, setSchema] = useState({});
-//   const [ui, setUi] = useState({});
-
-//   useEffect(() => {
-//     try {
-//       const parsedUiSchema = JSON.parse(uiSchema);
-//       setUi(parsedUiSchema);
-//       // Assuming you have schema and formData in your UI schema
-//       setSchema(parsedUiSchema.schema);
-//       setFormData(parsedUiSchema.formData);
-//     } catch (error) {
-//       console.error('Error parsing UI schema:', error);
-//       // Handle the error as needed
-//     }
-//   }, [uiSchema]);
-
-//   const handleSubmit = ({ formData }) => {
-//     console.log('Form Data Submitted:', formData);
-//     // Implement logic to send formData to the backend
-//   };
-
-//   return (
-//     <div style={{ width: '50%', padding: '20px' }}>
-//       <h2>Form Renderer</h2>
-//       <Form
-//         schema={schema}
-//         uiSchema={ui}
-//         formData={formData}
-//         onSubmit={handleSubmit}
-//       />
-//     </div>
-//   );
-// };
-
-// export default FormGenerator;
-
-
-// import React, { useState, useEffect } from 'react';
-// import Form from 'react-jsonschema-form';
-
-// const FormGenerator = ({ uiSchema }) => {
-//   const [formData, setFormData] = useState({});
-//   const [schema, setSchema] = useState({});
-//   const [ui, setUi] = useState({});
-
-//   useEffect(() => {
-//     try {
-//       const parsedUiSchema = JSON.parse(uiSchema);
-//       setUi(parsedUiSchema);
-//       // Assuming you have schema and formData in your UI schema
-//       setSchema(parsedUiSchema.schema);
-//       setFormData(parsedUiSchema.formData);
-//     } catch (error) {
-//       console.error('Error parsing UI schema:', error);
-//       // Handle the error as needed
-//     }
-//   }, [uiSchema]);
-
-//   const handleSubmit = ({ formData }) => {
-//     console.log('Form Data Submitted:', formData);
-//     // Implement logic to send formData to the backend
-//   };
-
-//   return (
-//     <div style={{ width: '50%', padding: '20px' }}>
-//       <h2>Form Renderer</h2>
-//       <Form
-//         schema={schema}
-//         uiSchema={ui}
-//         formData={formData}
-//         onSubmit={handleSubmit}
-//       />
-//     </div>
-//   );
-// };
-
-// export default FormGenerator;
-
-
-// src/components/FormGenerator.js
 import React, { useState, useEffect } from 'react';
 import Form from 'react-jsonschema-form';
 
-// src/components/FormGenerator.js
-// ... (other imports and code)
+const recursiveRender = (fields, currentTab = null, selectedPizzaType = null) => {
+  let acc = {};
 
-const recursiveRender = (fields) => {
-  return {
-    type: 'object',
-    properties: fields.reduce((acc, field) => {
-      if (field.uiType === 'Group') {
-        const groupFields = field.subParameters ? recursiveRender(field.subParameters) : {};
+  fields.forEach(field => {
+    if (field.uiType === 'Group') {
+      const groupFields = field.subParameters ? recursiveRender(field.subParameters, currentTab, selectedPizzaType) : {};
+      acc[field.jsonKey] = {
+        type: 'object',
+        properties: groupFields.properties,
+      };
+    } else if (field.uiType === 'Radio') {
+      acc[field.jsonKey] = {
+        type: 'string',
+        title: field.label,
+        enum: field.validate.options.map(option => option.value),
+        enumNames: field.validate.options.map(option => option.label),
+        // Added uiType and radioType for rendering radio buttons
+        uiType: 'Radio',
+        radioType: field.uiType,
+      };
+    } else if (field.uiType === 'Ignore' && field.conditions) {
+      const condition = field.conditions.find(cond => cond.action === 'enable');
+      if (condition && condition.value === currentTab) {
+        const ignoreGroupFields = field.subParameters ? recursiveRender(field.subParameters, currentTab, selectedPizzaType) : {};
         acc[field.jsonKey] = {
           type: 'object',
-          properties: groupFields.properties,
+          properties: ignoreGroupFields.properties,
         };
-      } else {
-        acc[field.jsonKey] = { type: 'string', title: field.label };
-        if (field.uiType.toLowerCase() === 'select') {
-          acc[field.jsonKey].enum = field.validate.options.map(option => option.value);
-          acc[field.jsonKey].enumNames = field.validate.options.map(option => option.label);
-        }
       }
-      return acc;
-    }, {}),
+    } else if (field.uiType === 'Input') {
+      acc[field.jsonKey] = { type: 'string', title: field.label };
+    } else if (field.uiType === 'Select' || field.jsonKey === 'slices') {
+      acc[field.jsonKey] = {
+        type: 'string',
+        title: field.label,
+        enum: field.validate.options.map(option => option.value),
+        enumNames: field.validate.options.map(option => option.label),
+      };
+    }
+  });
+
+  return {
+    type: 'object',
+    properties: acc,
   };
 };
-
-// ... (rest of the component code)
 
 
 const FormGenerator = ({ uiSchema }) => {
